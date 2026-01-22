ffbfc1d6f1c2d64724e98beec67b0ead6eb6adc3
hmcnulty <hgmcnult@uvm.edu>
2026-01-22 14:18:23 -0500

    notes jan 22

--- r_scripts/atomicVecs.R
+++ r_scripts/atomicVecs.R
@@ -16,7 +16,7 @@ y = 3 #also makes a value but, only stores the number within a function, it work
 plant_height <- 3 #this is called snake case ___ this is what i use!
 plantHeight <- 4 #this is camel case 
 plant.height <- 2 #not preferred but can do it in a pinch
-. #reserve for a temporary variable
+#. #reserve for a temporary variable
 
 
 #four basic data types in R
@@ -39,6 +39,110 @@ is.numeric(z) #yes
 is.character(z) #no
 
 
-#character strings
+#character strings " "
+
+t <- "perch"
+print(t)
+
+t <- c("perch", "bass", "trout") #perch is the 1st position
+print(t)
+t[1]
+t[3]
 
 
+t <- c("this is a 'character' string", "a second") #actually two parts
+t[1] #pulls the first part
+t[2] #pulls the second
+
+typeof(t)
+is.character(t)
+is.numeric(t)
+
+
+# Logical/Boolean 
+
+z <- c(TRUE, FALSE, TRUE)
+z
+
+is.logical(z)
+is.integer(z)
+typeof(z)
+
+
+c(T, F) #can be made with the first capital letter
+
+# true=1 false=0 so we can use it to do math, as below
+mean(z) #0.6666667
+
+
+# three properties of a vector
+
+#type
+z <- c(1.1, 1.2, 1.3, 1.4)
+typeof(z)
+is.numeric(z)
+t <- as.character(z) #coerces variable from a numeric to a character
+
+print(t)
+typeof(t)
+
+t <- c(1, 2, "3", 4) #if it sees one character, it will make the entire thing characters
+
+
+#length
+length(t)
+
+#naming
+
+
+#random number generator
+z <- runif(5) #its going to default to numbers between 0 and 1
+
+names(z) #NULL there are no names
+
+names(z) <- c("A", "B", "C", "D", "E") #added names
+
+print(z)
+
+names(t) <- NULL #resets the names back to nothing
+
+
+# special data types
+
+z <- c(3.2, 3, 3, NA)
+z
+
+typeof(z)
+length(z)
+typeof(z[3]) #asking what the third position of z is
+typeof(z[4])
+
+sum(z) #returns value NA
+
+sum(z, na.rm=T) #this removed NA and does the calculation
+
+
+z <- 0/0
+z
+
+
+z <- 1/0
+z
+
+
+#vectorization
+z <- c(10, 20, 30)
+
+z + 1
+z / 3
+
+y <- c(1, 2, 3)
+
+z + y
+
+
+# recycling coercion 
+
+x <- c(1, 2)
+
+z + x # this recycled so 10+1= 11 and then 20+2=22, BUT there is no other number so it does the 1 again 30+1=31

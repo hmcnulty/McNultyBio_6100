3afa6091b33539c82658929e9f17f05e6a53a6fd
hmcnulty <hgmcnult@uvm.edu>
2026-02-10 14:15:33 -0500

    Create for_loops.R

---
+++ r_scripts/for_loops.R
@@ -0,0 +1,18 @@
+# basic coding with for loops
+# february 10th, 2026
+# Hannah Grace McNulty
+
+
+# creating a basic for loop:
+
+for (i in 1:5) { #declared a loop of 1-5
+  cat("stuck in a loop", "\n") # the "\n" moves it to the next line in the console
+  cat(3 + 2, "\n")
+  cat(runif(1), "\n")
+}
+
+my_dogs <- c("chow", "akita", "malamute", "husky", "samoyed")
+
+for (i in 1:length(my_dogs)){
+  cat("i =", i, "my_dogs[i] =", my_dogs[i], "\n")
+}

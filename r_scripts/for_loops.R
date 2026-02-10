13968abc0c591b6e236eec82897c9ef1cff1e62a
hmcnulty <hgmcnult@uvm.edu>
2026-02-10 14:20:01 -0500

    Update for_loops.R

--- r_scripts/for_loops.R
+++ r_scripts/for_loops.R
@@ -16,3 +16,15 @@ my_dogs <- c("chow", "akita", "malamute", "husky", "samoyed")
 for (i in 1:length(my_dogs)){
   cat("i =", i, "my_dogs[i] =", my_dogs[i], "\n")
 }
+
+
+my_bad_dogs <- NULL 
+for (i in 1:length(my_bad_dogs)){
+    cat("i =", i, "my_bad_dogs[i] =", my_bad_dogs[i], "\n")
+
+}
+
+for (i in seq_along(my_dogs)){
+    cat("i =", i, "my_bad_dogs[i] =", my_bad_dogs[i], "\n")
+
+}

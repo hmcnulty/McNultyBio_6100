58dbe7a6e70830c0aced9246c19a7afe8bd8c220
hmcnulty <hgmcnult@uvm.edu>
2026-02-24 13:28:09 -0500

    Create ggplot.R

---
+++ ggplot.R
@@ -0,0 +1,171 @@
+
+
+p1 <- ggplot(data= <DATA>) +
+      aes(<MAPPINGS>) + 
+      <GEOM_FUNCTION>(aes(<MAPPINGS>),
+                      stat=<STAT>,
+                      position=<POSITION>) +
+                      <COORDINATE_FUNCTION> +
+                      <FACET_FUNCTION>
+
+print(p1)
+ggsave(plot=p1, 
+       filename="MyPlot",
+       width=5,
+       height=3,
+       units="in",
+       device="pdf")
+
+
+library(ggplot2)
+library(ggthemes)
+library(patchwork)
+
+d <- mpg # use built in mpg data frame
+str(d)
+
+table(d$fl)
+
+# basic histogram plot
+ ggplot(data=d) +
+      aes(x=hwy) +
+      geom_histogram()
+
+
+ggplot(data=d) + 
+   aes(x=hwy) + 
+   geom_histogram(fill="khaki",color="black")
+
+
+# # basic density plot
+ggplot(data=d) +
+  aes(x=hwy) +
+  geom_density(fill="mintcream",color="blue")
+
+
+# basic scatter plot
+ggplot(data=d) +
+  aes(x=displ,y=hwy) +
+  geom_point() +
+  geom_smooth() +
+  geom_smooth(method="lm",col="red")
+
+# add a smoother
+ggplot(data=d) +
+  aes(x=displ,y=hwy) +
+  geom_point() +
+  geom_smooth()
+
+# add a linear regression line
+ggplot(data=d) +
+  aes(x=displ,y=hwy) +
+  geom_point() +
+  geom_smooth(method = "lm",col="red")
+
+# basic boxplot
+ggplot(data=d) +
+  aes(x=fl, y=cty) +
+  geom_boxplot()
+
+# basic boxplot
+ggplot(data=d) +
+  aes(x=fl, y=cty) +
+  geom_boxplot(fill="thistle")
+
+# basic barplot (long format)
+
+ggplot(data=d) +
+  aes(x=fl) +
+  geom_bar(fill="thistle",color="black")
+
+
+# bar plot with specified counts or meansw
+ x_treatment <- c("Control","Low","High")
+ y_response <- c(12,2.5,22.9)
+ summary_data <- data.frame(x_treatment,y_response)
+ 
+ ggplot(data=summary_data) +
+   aes(x=x_treatment,y=y_response) +
+   geom_col(fill=c("grey50","goldenrod","goldenrod"),col="black")
+
+   # basic curves and functions
+ my_vec <- seq(1,100,by=0.1)
+ 
+ # plot simple mathematical functions
+ d_frame <- data.frame(x=my_vec,y=sin(my_vec))
+ ggplot(data=d_frame) +
+   aes(x=x,y=y) +
+   geom_line()
+
+ # plot probability functions
+ d_frame <- data.frame(x=my_vec,y=dgamma(my_vec,shape=5, scale=3))
+ ggplot(data=d_frame) +
+   aes(x=x,y=y) +
+   geom_line()
+
+
+   # plot user-defined functions
+ my_fun <- function(x) sin(x) + 0.1*x
+ d_frame <- data.frame(x=my_vec,y=my_fun(my_vec))
+ ggplot(data=d_frame) +
+   aes(x=x,y=y) +
+   geom_line()
+
+p1 <- ggplot(data=d, mapping=aes(x=displ,y=cty)) + geom_point()
+ print(p1)
+
+  # use theme parameters to modify font and font size
+ p1 + theme_classic(base_size=30,base_family="serif")
+
+
+  # defaults: theme_grey, base_size=16,base_family="Helvetica")
+ # font families (Mac): Times, Ariel, Monaco, Courier, Helvetica, serif,sans
+
+
+
+# code for adding additional fonts
+library(extrafont)
+font_import()  # Imports all system fonts (run once)
+
+# use coordinate_flip to invert entire plot
+ p2 <- ggplot(data=d, mapping=aes(x=fl,fill=fl)) + geom_bar()
+ print(p2)
+
+
+  p2 + coord_flip() + theme_grey(base_size=20,base_family="sans")
+
+
+  # use labs for different plot labels
+ p1 <- ggplot(data=d) +
+  aes(x=displ,y=cty) + 
+  geom_point() +
+ labs(title="My graph title here",
+      subtitle="An extended subtitle",
+      x="Displacement",
+      y="City Mileage",
+      caption="Add a caption here") +
+   theme_bw(base_size=25,base_family="Monaco")
+  #    xlim(0,4) + ylim(0,20)
+ print(p1)
+
+
+  # use attributes for point size shape, color
+ p1 <- ggplot(data=d) +
+  aes(x=displ,y=cty) + 
+ geom_point(size=4,
+            shape=21,
+            color="black",fill="cyan") +
+   theme_bw(base_size=25,base_family="Monaco")
+ print(p1)
+
+
+  # use x and/or y limits to clip data set
+ p1 <- ggplot(data=d) +
+  aes(x=displ,y=cty) + 
+ geom_point(size=4,
+            shape=21,
+            color="black",fill="cyan") +
+   xlim(4,7) + 
+   ylim(-10,40) +
+   theme_bw(base_size=25,base_family="Monaco")
+ print(p1)
